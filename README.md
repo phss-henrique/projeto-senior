803839be7a5c27c924f4e0fb33839b0b92e02eb0
phss-henrique <pedrohenrique2937@gmail.com>
2026-07-09 16:36:04 -0300

    errei-kin

---
+++ README.md
@@ -0,0 +1,3 @@
+# pix do mota
+
+para auxiliar aos que nao se recordam do numero do pix de motastico
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const PIX_NUMERO = '42904694854'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="title">pix do mota</h1>
+        <p className="numero">{PIX_NUMERO}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
