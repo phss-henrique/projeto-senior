76f194a0f824f8c857721436c147dc7f905bb093
PEDRO HENRIQUE DA SILVA DOS SANTOS <pedro.santos558@senaisp.edu.br>
2026-07-31 08:10:02 -0300

    peter-addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 
 const PIX_NUMERO = '42904694854'
+const PIX_PETER = 'pedrohenrique2937@gmail.com'
 
 function App() {
   return (
     <div className="container">
+
       <div className="card">
-        <h1 className="title">pix do mota</h1>
-        <p className="numero">{PIX_NUMERO}</p>
+        <section>
+          <h1 className="title">pix do mota</h1>
+          <p className="numero">{PIX_NUMERO}</p>
+        </section>
+
+        <section>
+          <h1 className="title">pix do peter</h1>
+          <p className="numero">{PIX_PETER}</p>
+        </section>
       </div>
+
     </div>
   )
 }
